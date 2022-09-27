ea54c1befae547cd838b905ce67dd43acac955b2
Christine Han <christinehan726@gmail.com>
2022-09-26 21:26:58 -0700

    flask forms and user input

--- flaskblog.py
+++ flaskblog.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
-app.config['SECRET KEY'] = 'da4667d21051f23286bd5bd63219d645'
+app.config['SECRET_KEY'] = 'da4667d21051f23286bd5bd63219d645'
 
 posts = [
     {
@@ -22,20 +22,35 @@ posts = [
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', posts= posts)
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-    return render_template('about.html', title= 'About')
+    return render_template('about.html', title='About')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if (
+            form.email.data == 'admin@blog.com'
+            and form.password.data == 'password'
+        ):
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash(
+                'Login Unsuccessful. Please check username and password',
+                'danger'
+            )
     return render_template('login.html', title='Login', form=form)
 
 
