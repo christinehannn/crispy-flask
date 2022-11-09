4542300ab91e6c18bdb3e90ba28adea74a228d96
Christine Han <christinehan726@gmail.com>
2022-11-08 22:05:44 -0800

    added error handling

--- flaskblog/posts/routes.py
+++ flaskblog/posts/routes.py
@@ -32,7 +32,7 @@ def new_post():
         db.session.add(post)
         db.session.commit()
         flash('Your post has been created!', 'success')
-        return redirect(url_for('posts.home'))
+        return redirect(url_for('main.home'))
     return render_template(
         'create_post.html',
         title='New Post',
@@ -80,4 +80,4 @@ def delete_post(post_id):
     db.session.delete(post)
     db.session.commit()
     flash('Your post has been deleted!', 'success')
-    return redirect(url_for('posts.home'))
+    return redirect(url_for('main.home'))
