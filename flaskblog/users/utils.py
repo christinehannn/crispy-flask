b35cd0f6e9646f4f6033c096d6e830e25ac90f93
Christine Han <christinehan726@gmail.com>
2022-12-02 14:18:15 -0800

    fixed url reset token

--- flaskblog/users/utils.py
+++ flaskblog/users/utils.py
@@ -32,7 +32,7 @@ def send_reset_email(user):
     token = user.get_reset_token()
     body = (
         f'To reset your password, visit the following link: '
-        f'{url_for("reset_token", token=token, _external=True)}\n'
+        f'{url_for("users.reset_token", token=token, _external=True)}\n'
         'If you did not make this request then simply ignore this email and '
         'no changes will be made.'
     )
